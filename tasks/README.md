389ba42531f7ccf2973a293c535132984788da92
kaellandrade <micael.santos@dcomp.ufs.br>
2021-08-05 18:18:03 -0300

    ajuste estilos

---
+++ tasks/README.md
@@ -0,0 +1,8 @@
+# Task List
+
+
+# Telas
+
+# Referências
+
+[Freepik 📷](https://br.freepik.com/vetores-gratis/lista-de-verificacao-ou-conceito-de-pesquisa_9649176.htm#page=1&query=task&position=17)
--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground } from 'react-native';
+import TodayImage from '../../assets/imgs/today.png';
+import comonStyles from '../comonStyles';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
 /**
  * Componente principal.
  */
 class TaskList extends Component {
     render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
-            <View>
-                <Text>
-                    Projeto Taks!
-                </Text>
+            <View style={styles.container}>
+                <ImageBackground style={styles.backGround} source={TodayImage}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.SubTitle} >{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text></Text>
+                </View>
             </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    backGround: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        alignItems: "flex-start"
+    },
+    title: {
+        fontFamily: 'Lato',
+        fontSize: 35,
+        color: comonStyles.colors.secundary,
+        marginBottom: 20,
+        marginLeft: 20
+    },
+    SubTitle: {
+        fontFamily: 'Lato',
+        fontSize: 20,
+        color: comonStyles.colors.secundary,
+        marginBottom: 20,
+        marginLeft: 20
+    }
+});
 
 export default TaskList;
