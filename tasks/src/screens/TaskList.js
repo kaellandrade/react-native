db7c443ec041ce383dcf6274e012d6e3f3fe2695
kaellandrade <micael.santos@dcomp.ufs.br>
2021-08-05 16:59:35 -0300

    iniciando o projeto tasklist

---
+++ tasks/src/screens/TaskList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+/**
+ * Componente principal.
+ */
+class TaskList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Projeto Taks!
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default TaskList;
