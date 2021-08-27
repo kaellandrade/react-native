bfa1ac3db425f70b2ea63d6b3fe3f49f6b283b5d
kaellandrade <micael.santos@dcomp.ufs.br>
2021-08-27 12:01:27 -0300

    projeto lambe lambe

---
+++ lambe/src/components/Author.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import { Gravatar } from 'react-native-gravatar';
+
+const Author = props => {
+    return (
+        <View style={styles.container}>
+            <Gravatar options={{ email: props.email, secure: true }} style={styles.avatar} />
+            <Text style={styles.nickname}>{props.nickname}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    nickname: {
+        color: '#444',
+        marginVertical: 10,
+        fontWeight: 'bold',
+        fontSize: 15
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        marginHorizontal: 15
+    }
+
+})
+
+export default Author;
