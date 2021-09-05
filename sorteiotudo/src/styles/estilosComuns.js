b62725d58ba7e05e6489e1cff0b7277ae20e8143
kaellandrade <micael.santos@dcomp.ufs.br>
2021-09-05 14:12:48 -0300

    configurando o projeto amigo secreto

---
+++ sorteiotudo/src/styles/estilosComuns.js
@@ -0,0 +1,27 @@
+const ESTILOS_COMUNS = {
+    fontPrincipal: {
+        regular: 'Roboto-Regular',
+        light: 'Roboto-Light',
+        thin: 'Roboto-Thin',
+        bold: 'Roboto-Bold',
+        medium: 'Roboto-Medium'
+    },
+    fontLogo: 'Righteous-Regular',
+    cores: {
+        principal: '#222',
+        secundaria: '#f1f0f1',
+        perigo: '#f94144',
+        cuidado: '#f9c74f',
+        sucesso: '#90be6d',
+        larajan: '#f8961e',
+        amizade: '#f9844a',
+        azulSecundario: '#4d908e',
+        azulPrimario: '#577590'
+    },
+    iconesTamanhos: {
+        pequeno: 10,
+        medio: 20,
+        grande: 30
+    }
+}
+export { ESTILOS_COMUNS }
